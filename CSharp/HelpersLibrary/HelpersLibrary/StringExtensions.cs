994d56f6cc1aee432f845f51d61905ee1ec6ab7d
Devid Pavliček <pavlicekdavid@hotmail.com>
2020-09-06 22:00:23 +0200

    TypeScript
    Added tsconfig and folders

---
+++ CSharp/HelpersLibrary/HelpersLibrary/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace HelpersLibrary
+{
+    using System.Globalization;
+
+    public static class StringExtensions
+    {
+        public static int ToInt(this string value, int defaultValue)
+        {
+            return int.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out int result) ? result : defaultValue;
+        }
+
+        public static long ToLong(this string value, long defaultValue)
+        {
+            return long.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out long result) ? result : defaultValue;
+        }
+
+        public static decimal ToDecimal(this string value, decimal defaultValue)
+        {
+            return decimal.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out decimal result) ? result : defaultValue;
+        }
+    }
+}
