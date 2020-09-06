696305559b336d85f7f26366e36709397b7ed126
Devid Pavliček <pavlicekdavid@hotmail.com>
2020-09-06 20:47:04 +0200

    Added Csharp String Extensions
    Added two folders for csharp and typescript. And added csharp string extensions

---
+++ CSharp/HelpersLibrary/ExtensionsLibrary/StringExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExtensionsLibrary
+{
+    public static class StringExtensions
+    {
+        public static int ToInt(this string value, int defaultValue)
+        {
+            if (int.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out var result))
+            {
+                return result;
+            }
+
+            return defaultValue;
+        }
+
+        public static long ToLong(this string value, long defaultValue)
+        {
+            if (long.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out var result))
+            {
+                return result;
+            }
+
+            return defaultValue;
+        }
+
+        public static decimal ToDecimal(this string value, decimal defaultValue)
+        {
+            if (decimal.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out var result))
+            {
+                return result;
+            }
+
+            return defaultValue;
+        }
+
+    }
+}
