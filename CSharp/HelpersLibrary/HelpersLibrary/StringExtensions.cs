6d3d257ec18250e47c811f4ba99b214623c71a20
Devid Pavliček <pavlicekdavid@hotmail.com>
2020-09-20 21:58:25 +0200

    CSharp String Extensions
    Added JoinMultiline and Limit Method

--- CSharp/HelpersLibrary/HelpersLibrary/StringExtensions.cs
+++ CSharp/HelpersLibrary/HelpersLibrary/StringExtensions.cs
@@ -1,5 +1,6 @@
 ﻿namespace HelpersLibrary
 {
+    using System;
     using System.Globalization;
 
     public static class StringExtensions
@@ -17,6 +18,51 @@
         public static decimal ToDecimal(this string value, decimal defaultValue)
         {
             return decimal.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out decimal result) ? result : defaultValue;
+        }
+
+        /// <summary>Limits the specified string. </summary>
+        /// <param name="text">The string. </param>
+        /// <param name="numberOfCharacters">The number of characters. </param>
+        /// <returns>Limited string. </returns>
+        public static string Limit(this string text, int numberOfCharacters)
+        {
+            numberOfCharacters = Math.Abs(numberOfCharacters);
+            if (string.IsNullOrEmpty(text))
+            {
+                return text;
+            }
+
+            if (numberOfCharacters == 0)
+            {
+                return string.Empty;
+            }
+
+            if (text.Length <= numberOfCharacters)
+            {
+                return text;
+            }
+
+            return text.Substring(0, numberOfCharacters);
+        }
+
+        public static string JoinMultiline(this string[] values, string separator = "\r\n")
+        {
+            if (values != null && values.Length > 0)
+            {
+                return string.Join(separator, values);
+            }
+
+            return string.Empty;
+        }
+
+        public static string[] SplitMultiline(this string value, StringSplitOptions splitOptions = StringSplitOptions.None)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                return value.Split(new[] { Environment.NewLine }, splitOptions);
+            }
+
+            return new string[0];
         }
     }
 }
